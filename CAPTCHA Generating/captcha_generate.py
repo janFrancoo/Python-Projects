3cfe50b363e32e67c6142bccd63e762d7efd45ee
Eren Sırakaya <45636602+janFrancoo@users.noreply.github.com>
2019-07-27 19:30:51 +0300

    Create captcha_generate.py

---
+++ CAPTCHA Generating/captcha_generate.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+             'v', 'w', 'x', 'y', 'z']
+uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+             'V', 'W', 'X', 'Y', 'Z']
+
+
+def create_captcha(generate=1, size=5):
+    for captcha in range(generate):
+        white_bg = np.ones((50, 150)) * 255
+        pos_x = 0
+        pos_y = 30
+        real = ""
+        for i in range(size):
+            char = np.random.randint(0, 3)
+            if char == 0:
+                num = np.random.randint(0, 10)
+                thickness = np.random.randint(2, 7)
+                cv2.putText(white_bg, str(num), (pos_x, pos_y), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, thickness)
+                real += str(num)
+            if char == 1:
+                num = np.random.randint(0, 26)
+                thickness = np.random.randint(2, 7)
+                cv2.putText(white_bg, lowercase[num], (pos_x, pos_y), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, thickness)
+                real += lowercase[num]
+            if char == 2:
+                num = np.random.randint(0, 26)
+                thickness = np.random.randint(2, 7)
+                cv2.putText(white_bg, uppercase[num], (pos_x, pos_y), cv2.FONT_HERSHEY_SIMPLEX, 1, 0, thickness)
+                real += uppercase[num]
+            pos_x += int(150/size)
+        for i in range(size * 2):
+            line_pos_x1 = np.random.randint(0, 150)
+            line_pos_x2 = np.random.randint(0, 150)
+            line_pos_y1 = np.random.randint(0, 50)
+            line_pos_y2 = np.random.randint(0, 50)
+            increment1 = np.random.randint(0, 50)
+            increment2 = np.random.randint(0, 50)
+            cv2.line(white_bg, (line_pos_x1, line_pos_x2), (line_pos_y1 + increment1, line_pos_y2 + increment2), 0, 2)
+        print(real)
+        cv2.imshow('white', white_bg)
+        cv2.waitKey(0)
+
+
+create_captcha(10, 7)
